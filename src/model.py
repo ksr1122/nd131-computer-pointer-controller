e6ff231c71db37bcc28e6bac6ad515742b1c0f91
ravi_ks <ksr1122@gmail.com>
2020-05-10 11:20:41 +0200

    feat: implement base model constructor
     * instantiate Inference Engine core
     * read network from model structure & weight
     * add extension if needed
     * setup input's & output's name & shape

--- src/model.py
+++ src/model.py
@@ -1,17 +1,41 @@
+import os
+import cv2
+
+import logging as log
+
+from openvino.inference_engine import IENetwork, IECore
+
+
 '''
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
 
-class Model_X:
+class Model:
     '''
-    Class for the Face Detection Model.
+    Base Class Model.
     '''
-    def __init__(self, model_name, device='CPU', extensions=None):
-        '''
-        TODO: Use this to set your instance variables.
-        '''
-        raise NotImplementedError
+    def __init__(self, model_name, device='CPU', extensions=None, threshold):
+        self.device = device
+        self.extension = extension
+        self.threshold = threshold
+        self.model_name = model_name
+
+        try:
+            model_weights = model_name + '.bin'
+            model_structure = model_name + '.xml'
+            self.core = IECore()
+            self.model = self.core.read_network(model_structure, model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+
+        if extension and "CPU" in device:
+            self.core.add_extension(extension, device)
+
+        self.input_name = next(iter(self.model.inputs))
+        self.input_shape = self.model.inputs[self.input_name].shape
+        self.output_name = next(iter(self.model.outputs))
+        self.output_shape = self.model.outputs[self.output_name].shape
 
     def load_model(self):
         '''
